76a4ff2da530e3961a6680dc6ecb15aab375d542
Hemant Kaushik <TheHemantKaushik@gmail.com>
2024-12-12 16:16:50 +0530

    Basic files added

--- examples/lib/main.dart
+++ examples/lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World! ${calc.addOne(100)}'),
+          child: Text('Hello World! ${calc.addOne(1)}'),
         ),
       ),
     );
